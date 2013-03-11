0604cb90968023a0f4c7578c38d858deac61cca2
Ash McKenzie <ash@greenworm.com.au>
2013-03-11 22:33:57 +1100

    Support for local_application.css and emoji for static

--- lib/gloat/static.rb
+++ lib/gloat/static.rb
@@ -5,14 +5,8 @@ module Gloat
   class Static
 
     def generate
-
-      # Index
       generate_index
-
-      # Decks
-      config.static_decks.each do |deck|
-        generate_deck deck
-      end
+      config.static_decks.each { |deck| generate_deck deck }
     end
 
     def generate_index
@@ -60,14 +54,12 @@ module Gloat
         markup = Nokogiri::HTML(deck.render)
 
         # CSS
-        #
         markup.css('link').each do |x|
           next if !x.attribute('href') || x.attribute('href').value.match(/^http/)
           x.attribute('href').value = "../.." + x.attribute('href').value
         end
 
         # JavaScript
-        #
         markup.css('script').each do |x|
           next if !x.attribute('src') || x.attribute('src').value.match(/^http/)
           x.attribute('src').value = "../.." + x.attribute('src').value
@@ -76,14 +68,23 @@ module Gloat
         file.write markup.to_s
       end
 
-      %W{ vendor.js application.js base.css vendor.css application.css themes/#{deck.theme}/style.css themes/#{deck.theme}/background.png themes/#{deck.theme}/logo.png }.each do |file|
+      %W{
+        vendor.js
+        application.js
+        base.css
+        vendor.css
+        application.css
+        local_application.css
+        themes/#{deck.theme}/style.css
+        themes/#{deck.theme}/background.png
+        themes/#{deck.theme}/logo.png
+      }.each do |file|
         File.open(File.join(static_path, 'assets', file), 'w') do |f|
           f.write env[file].to_s
         end
       end
 
       # Slide images
-      #
       FileUtils.cp_r images_path, File.join(static_path, 'images') if Dir.exist?(images_path)
 
       # Theme files
@@ -91,8 +92,8 @@ module Gloat
       FileUtils.cp_r theme_files, File.join(static_path, 'assets', 'themes', deck.theme)
 
       # Assets
-      #
       FileUtils.cp_r Dir[File.join(assets_path, 'images', '*')], File.join(static_path, 'assets')
+      FileUtils.cp_r Dir[File.join(root_assets_path, 'images', '*')], File.join(static_path, 'assets')
 
       FileUtils.cp_r Dir[File.join(assets_path, 'fonts', '*')], File.join(static_path, 'assets')
       FileUtils.cp_r Dir[File.join(root_assets_path, 'fonts', '*')], File.join(static_path, 'assets')
